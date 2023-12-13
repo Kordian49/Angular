386836447c10ea0583764c280f7ce6dd543cba0a
Kordian <kordian.niezgoda@gmail.com>
2023-12-13 10:00:06 +0100

    init commit

---
+++ src/app/housinglocation.ts
@@ -0,0 +1,11 @@
+export interface HousingLocation {
+  id: number;
+  name: string;
+  city: string;
+  street: string;
+  photo: string;
+  room: number;
+  wifi: boolean;
+  centum: boolean;
+  price:boolean;
+}
